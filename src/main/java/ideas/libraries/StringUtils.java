0223bbfb8b47e075a7c079c2aa0bea69925cc4a3
natanmacielc <natan.maciel@gmail.cim>
2022-08-09 12:55:36 -0300

    StringUtils

---
+++ src/main/java/ideas/libraries/StringUtils.java
@@ -0,0 +1,29 @@
+package ideas.libraries;
+
+public class StringUtils {
+
+    public static String capitalize(String str) {
+        int strLen;
+        if (str != null && (strLen = str.length()) != 0) {
+            int firstCodepoint = str.codePointAt(0);
+            int newCodePoint = Character.toTitleCase(firstCodepoint);
+            if (firstCodepoint == newCodePoint) {
+                return str;
+            } else {
+                int[] newCodePoints = new int[strLen];
+                int outOffset = 0;
+                newCodePoints[outOffset++] = newCodePoint;
+
+                int codepoint;
+                for(int inOffset = Character.charCount(firstCodepoint); inOffset < strLen; inOffset += Character.charCount(codepoint)) {
+                    codepoint = str.codePointAt(inOffset);
+                    newCodePoints[outOffset++] = codepoint;
+                }
+
+                return new String(newCodePoints, 0, outOffset);
+            }
+        } else {
+            return str;
+        }
+    }
+}
